e979ead504bdbe0495f8b7b7d263d9fe97991b39
Tom De Vylder <tom@penumbra.be>
2012-08-02 12:43:23 +0200

    Add pnp4nagios support for debian

--- manifests/plugins/pnp4nagios/install.pp
+++ manifests/plugins/pnp4nagios/install.pp
@@ -1,12 +1,24 @@
 class icinga::plugins::pnp4nagios::install {
+  case $::operatingsystem {
+    /Debian|Ubuntu/: {
+      $package = $::lsbdistcodename ? {
+        'squeeze' => 'backports-squeeze/pnp4nagios',
+        default   => 'pnp4nagios',
+      }
+    }
 
-  $package = $::operatingsystem ? {
-    /Debian|Ubuntu/                       => 'pnp4nagios',
-    /CentOS|RedHat|Scientific|OEL|Amazon/ => 'pnp4nagios',
+    /CentOS|RedHat|Scientific|OEL|Amazon/: {
+      $package = 'pnp4nagios'
+    }
+
+    default: {
+      fail "Wrong value for \$ensure: ${ensure}."
+    }
   }
 
   package { $package:
     ensure => present;
   }
+
 }
 
