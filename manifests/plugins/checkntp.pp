feb42cb29c596c60e14da5dc4d91e28c7be02a39
Dave Simons <dave@inuits.eu>
2012-11-14 16:31:37 +0100

    replace default check_ntp_time command with checkntp plugin class
    Signed-off-by: Dave Simons <dave@inuits.eu>

---
+++ manifests/plugins/checkntp.pp
@@ -0,0 +1,26 @@
+class icinga::plugins::checkntp (
+  ntp_server         = 'pool.ntp.org',
+  warn_value         = '60',
+  crit_value         = '600',
+  max_check_attempts = $::icinga::max_check_attempts,
+) inherits ::icinga {
+
+  require ::ntp
+
+  file{"${::icinga::includedir_client}/ntp.cfg":
+    ensure  => 'file',
+    mode    => '0644',
+    owner   => $::icinga::client_user,
+    group   => $::icinga::client_group,
+    content => "command[check_ntp]=${::icinga::usrlib}/nagios/plugins/check_ntp -H ${ntp_server} -w ${warn_value} -c ${crit_value}\n",
+  }
+
+  @@nagios_service{"check_ntp_${::fqdn}":
+    check_command       => 'check_nrpe!check_ntp',
+    service_description => 'NTP Time Drift'
+    host_name           => $::fqdn,
+    max_check_attempts  => $max_check_attempts,
+    target              => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+  }
+
+}
