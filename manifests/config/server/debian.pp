754ef28f97c8dd44df971b70143c28e749e8bebd
Dave Simons <dave@inuits.eu>
2012-12-14 15:13:41 +0100

    dedup config::server::debian/redhat
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/config/server/debian.pp
+++ manifests/config/server/debian.pp
@@ -3,75 +3,38 @@
 # This class provides server configuration for Debian and derivative distro's.
 #
 class icinga::config::server::debian {
-  if $::icinga::server {
-    File {
-      owner   => $::icinga::server_user,
-      group   => $::icinga::server_group,
-      require => Class['icinga::config'],
-      notify  => [
-        Service[$::icinga::service_client],
-        Service[$::icinga::service_server],
-        Group[$::icinga::server_cmd_group]
-      ],
-    }
 
-    nagios_command {
-      'check_nrpe_command':
-        command_line => '$USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$',
-        target       => "${::icinga::targetdir}/commands/check_nrpe_command.cfg";
-    }
-
-    file {
-      $::icinga::confdir_server:
-        ensure => present;
-
-      $::icinga::htpasswd_file:
-        ensure => present,
-        mode   => '0644';
-
-      $::icinga::icinga_vhost:
-        ensure  => present,
-        content => template('icinga/debian/apache2.conf'),
-        notify  => Service[$::icinga::service_webserver];
-
-      "${::icinga::confdir_server}/objects":
-        ensure => directory;
-
-      "${::icinga::confdir_server}/objects/hosts":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::confdir_server}/objects/commands":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::confdir_server}/objects/services":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::confdir_server}/objects/contacts":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::confdir_server}/icinga.cfg":
-        ensure  => present,
-        content => template('icinga/debian/icinga.cfg');
-
-      "${::icinga::confdir_server}/cgi.cfg":
-        ensure  => present,
-        content => template('icinga/debian/cgi.cfg');
-
-      "${::icinga::confdir_server}/objects/generic-host_icinga.cfg":
-        ensure  => present,
-        content => template('icinga/debian/objects/generic-host_icinga.cfg');
-
-      "${::icinga::sharedir_server}/images/logos":
-        ensure  => directory;
-
-      "${::icinga::sharedir_server}/images/logos/os":
-        ensure  => directory,
-        recurse => true,
-        source  => 'puppet:///modules/icinga/img-os';
-    }
+  File {
+    ensure  => 'present',
+    owner   => $::icinga::server_user,
+    group   => $::icinga::server_group,
+    notify  => [
+      Service[$::icinga::service_client],
+      Service[$::icinga::service_server],
+      Group[$::icinga::server_cmd_group]
+    ],
   }
+
+  nagios_command{'check_nrpe_command':
+    command_line => '$USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$',
+    target       => "${::icinga::targetdir}/commands/check_nrpe_command.cfg",
+  }
+
+  file{$::icinga::icinga_vhost:
+    content => template('icinga/debian/apache2.conf'),
+    notify  => Service[$::icinga::service_webserver],
+  }
+
+  file{"${::icinga::confdir_server}/icinga.cfg":
+    content => template('icinga/debian/icinga.cfg'),
+  }
+
+  file{"${::icinga::confdir_server}/cgi.cfg":
+    content => template('icinga/debian/cgi.cfg'),
+  }
+
+  file{"${::icinga::confdir_server}/objects/generic-host_icinga.cfg":
+    content => template('icinga/debian/objects/generic-host_icinga.cfg'),
+  }
+
 }
--- manifests/config/server/redhat.pp
+++ manifests/config/server/redhat.pp
@@ -3,127 +3,94 @@
 # This class provides server configuration for RHEL and derivative distro's.
 #
 class icinga::config::server::redhat {
-  if $::icinga::server {
-    File {
-      owner   => $::icinga::server_user,
-      group   => $::icinga::server_group,
-      require => Class['icinga::config'],
-      notify  => [
-        Service[$::icinga::service_client],
-        Service[$::icinga::service_server],
-        Group[$::icinga::server_cmd_group],
-        Exec['fix_collected_permissions']
-      ],
-    }
 
-    exec { 'fix_collected_permissions':
-      # temporary work-around
-      command     => "/bin/chown -R ${::icinga::server_user}:${::icinga::server_group} .",
-      cwd         => $icinga::params::targetdir,
-      notify      => Service[$::icinga::service_server],
-      require     => File[$::icinga::targetdir],
-      refreshonly => true,
-    }
-
-    file {
-      $::icinga::icinga_vhost:
-        ensure  => present,
-        content => template('icinga/redhat/httpd.conf.erb'),
-        notify  => Service[$::icinga::service_webserver];
-
-      $::icinga::htpasswd_file:
-        ensure => present,
-        mode   => '0644';
-
-      $::icinga::confdir_server:
-        ensure  => directory,
-        recurse => true,
-        purge   => true;
-
-      $::icinga::vardir_server:
-        ensure => directory;
-
-      $::icinga::logdir_server:
-        ensure => directory;
-
-      $::icinga::targetdir:
-        ensure  => directory,
-        recurse => true,
-        purge   => true;
-
-      "${::icinga::targetdir}/hosts":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::targetdir}/contacts":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::targetdir}/services":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::targetdir}/commands":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::confdir_server}/modules":
-        ensure  => directory,
-        recurse => true;
-
-      "${::icinga::targetdir}/commands.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/commands.cfg.erb');
-
-      "${::icinga::targetdir}/notifications.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/notifications.cfg.erb');
-
-      "${::icinga::targetdir}/timeperiods.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/timeperiods.cfg.erb');
-
-      "${::icinga::targetdir}/templates.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/templates.cfg.erb');
-
-      "${::icinga::confdir_server}/cgi.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/cgi.cfg.erb');
-
-      "${::icinga::targetdir}/generic-host.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/generic-host.cfg');
-
-      "${::icinga::targetdir}/generic-service.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/generic-service.cfg');
-
-      "${::icinga::confdir_server}/icinga.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/icinga.cfg.erb');
-
-      "${::icinga::logdir_server}/archives":
-        ensure => directory;
-
-      "${::icinga::vardir_server}/rw":
-        ensure  => directory,
-        group   => $::icinga::server_cmd_group;
-
-      "${::icinga::vardir_server}/checkresults":
-        ensure => directory;
-
-      "${::icinga::confdir_server}/resource.cfg":
-        ensure  => present,
-        content => template('icinga/redhat/resource.cfg.erb');
-
-      "${::icinga::sharedir_server}/images/logos":
-        ensure  => directory;
-
-      "${::icinga::sharedir_server}/images/logos/os":
-        ensure  => directory,
-        recurse => true,
-        source  => 'puppet:///modules/icinga/img-os';
-    }
+  File {
+    ensure  => present,
+    owner   => $::icinga::server_user,
+    group   => $::icinga::server_group,
+    require => Class['icinga::config'],
+    notify  => [
+      Service[$::icinga::service_client],
+      Service[$::icinga::service_server],
+      Group[$::icinga::server_cmd_group],
+      Exec['fix_collected_permissions']
+    ],
   }
+
+  exec { 'fix_collected_permissions':
+    # temporary work-around
+    command     => "/bin/chown -R ${::icinga::server_user}:${::icinga::server_group} .",
+    cwd         => $icinga::params::targetdir,
+    notify      => Service[$::icinga::service_server],
+    require     => File[$::icinga::targetdir],
+    refreshonly => true,
+  }
+
+  file{$::icinga::icinga_vhost:
+    content => template('icinga/redhat/httpd.conf.erb'),
+    notify  => Service[$::icinga::service_webserver],
+  }
+
+  file{$::icinga::vardir_server:
+    ensure => directory,
+  }
+
+  file{$::icinga::logdir_server:
+    ensure => directory,
+  }
+
+  file{"${::icinga::confdir_server}/modules":
+    ensure  => directory,
+    recurse => true,
+  }
+
+  file{"${::icinga::targetdir}/commands.cfg":
+    content => template('icinga/redhat/commands.cfg.erb'),
+  }
+
+  file{"${::icinga::targetdir}/notifications.cfg":
+    content => template('icinga/redhat/notifications.cfg.erb'),
+  }
+
+  file{"${::icinga::targetdir}/timeperiods.cfg":
+    content => template('icinga/redhat/timeperiods.cfg.erb'),
+  }
+
+  file{"${::icinga::targetdir}/templates.cfg":
+    content => template('icinga/redhat/templates.cfg.erb'),
+  }
+
+  file{"${::icinga::confdir_server}/cgi.cfg":
+    content => template('icinga/redhat/cgi.cfg.erb'),
+  }
+
+  file{"${::icinga::targetdir}/generic-host.cfg":
+    content => template('icinga/redhat/generic-host.cfg'),
+  }
+
+  file{"${::icinga::targetdir}/generic-service.cfg":
+    content => template('icinga/redhat/generic-service.cfg'),
+  }
+
+  file{"${::icinga::confdir_server}/icinga.cfg":
+    content => template('icinga/redhat/icinga.cfg.erb'),
+  }
+
+  file{"${::icinga::logdir_server}/archives":
+    ensure => directory,
+  }
+
+  file{"${::icinga::vardir_server}/rw":
+    ensure  => directory,
+    group   => $::icinga::server_cmd_group,
+  }
+
+  file{"${::icinga::vardir_server}/checkresults":
+    ensure => directory,
+  }
+
+  file{"${::icinga::confdir_server}/resource.cfg":
+    content => template('icinga/redhat/resource.cfg.erb'),
+  }
+
 }
