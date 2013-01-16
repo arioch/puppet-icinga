d5f781f96f4e35e1635354eec5dfa0027eb01274
Dave Simons <dave@inuits.eu>
2013-01-16 18:01:08 +0100

    add check_bacula plugin
    Signed-off-by: Dave Simons <dave@inuits.eu>

---
+++ manifests/plugins/checkbacula.pp
@@ -0,0 +1,36 @@
+# == Class: icinga::plugins::checkbacula
+#
+# This class provides a checkbacula plugin.
+#
+class icinga::plugins::checkbacula (
+  $pkgname               = 'nagios-plugins-bacula',
+  $jobname               = $::fqdn,
+  $notification_period   = $::icinga::notification_period,
+  $notifications_enabled = $::icinga::notifications_enabled,
+) inherits icinga {
+
+  if $icinga::client {
+    package{$pkgname:
+      ensure => 'installed',
+    }
+
+    file{"${::icinga::includedir_client}/bacula.cfg":
+      ensure  => 'file',
+      mode    => '0644',
+      owner   => $::icinga::client_user,
+      group   => $::icinga::client_group,
+      content => "command[check_bacula]=<%= scope.lookupvar('icinga::plugindir') %>/check_bacula -j ${jobname} -w ${warning} -c ${critical}\n",
+    }
+
+    @@nagios_service{"check_bacula_${::fqdn}":
+      check_command         => 'check_nrpe_command!check_bacula',
+      service_description   => 'Bacula Jobs',
+      host_name             => $::fqdn,
+      use                   => 'generic-service',
+      notification_period   => $notification_period,
+      notifications_enabled => $notifications_enabled,
+      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+    }
+  }
+
+}
