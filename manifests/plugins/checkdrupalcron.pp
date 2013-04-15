d67c4eefddcedda641fcf49f4d011cc34fd72632
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-04-15 10:38:36 +0200

    checkdrupalcron: override for hostname
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/plugins/checkdrupalcron.pp
+++ manifests/plugins/checkdrupalcron.pp
@@ -7,8 +7,9 @@ class icinga::plugins::checkdrupalcron (
   $pkgname                = 'nagios-plugins-drupalcron',
   $notification_period    = $::icinga::notification_period,
   $notifications_enabled  = $::icinga::notifications_enabled,
-  $warning               = '0',
-  $critical              = '0',
+  $host_name              = ${::fqdn},
+  $warning                = '0',
+  $critical               = '0',
   $uri                    = '',
   $root                   = '',
   $warning_after_seconds  = '3600',
@@ -29,14 +30,14 @@ class icinga::plugins::checkdrupalcron (
       notify  => Service[$::icinga::service_client],
     }
 
-    @@nagios_service{"check_drupal_cron_${::fqdn}":
+    @@nagios_service{"check_drupal_cron_${host_name}":
       check_command         => 'check_nrpe_command!check_drupal_cron',
       service_description   => 'Check last Drupal cron update',
-      host_name             => $::fqdn,
+      host_name             => ${host_name},
       use                   => 'generic-service',
       notification_period   => $notification_period,
       notifications_enabled => $notifications_enabled,
-      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+      target                => "${::icinga::targetdir}/services/${host_name}.cfg",
     }
   }
 }
