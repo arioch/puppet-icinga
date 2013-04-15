ad3cd9ed68e83592c9ef411eb7ffbcb0d575b435
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-04-15 10:26:18 +0200

    Added more options and initial service layout
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/plugins/checkdrupalcron.pp
+++ manifests/plugins/checkdrupalcron.pp
@@ -4,10 +4,13 @@
 #
 # Warning an Critial expressed in seconds.  3600sec = 1h, 7200sec = 2h
 class icinga::plugins::checkdrupalcron (
+  $pkgname                = 'nagios-plugins-drupalcron',
   $notification_period    = $::icinga::notification_period,
   $notifications_enabled  = $::icinga::notifications_enabled,
-  $uri                    = undef,
-  $root                   = undef,
+  $warning               = '0',
+  $critical              = '0',
+  $uri                    = '',
+  $root                   = '',
   $warning_after_seconds  = '3600',
   $critical_after_seconds = '7200',
 ) inherits icinga {
@@ -24,6 +27,16 @@ class icinga::plugins::checkdrupalcron (
       group   => $::icinga::client_group,
       content => "command[check_drupal_cron]=${::icinga::plugindir}/check_drupal_cron ${uri} ${root} ${warning_after_seconds} ${critical_after_seconds}\n",
       notify  => Service[$::icinga::service_client],
+    }
+
+    @@nagios_service{"check_drupal_cron_${::fqdn}":
+      check_command         => 'check_nrpe_command!check_drupal_cron',
+      service_description   => 'Check last Drupal cron update',
+      host_name             => $::fqdn,
+      use                   => 'generic-service',
+      notification_period   => $notification_period,
+      notifications_enabled => $notifications_enabled,
+      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
     }
   }
 }
