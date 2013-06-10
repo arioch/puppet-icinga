b81f8cbafbcfa734ae77e9ae44e02e2a7f885a3c
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-06-10 17:11:30 +0200

    Icinga::Downtime with capital D
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/collect.pp
+++ manifests/collect.pp
@@ -26,7 +26,7 @@ class icinga::collect {
       notify => Service[$::icinga::service_server],
       target => "${::icinga::targetdir}/timeperiods.cfg",
     }
-    Icinga::downtime <<| |>> {  }
+    Icinga::Downtime <<| |>> {  }
   }
 
   if $::icinga::client {
