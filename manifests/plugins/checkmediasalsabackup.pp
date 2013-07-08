2a08eabe613eaffa3872bbedf424a62796701f4a
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-07-08 13:59:42 +0200

    Add check for Mediasalsa backups
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

---
+++ manifests/plugins/checkmediasalsabackup.pp
@@ -0,0 +1,47 @@
+# == Class: icinga::plugins::checkmediasalsabackup
+#
+# This class provides a checkmediasalsabackup plugin.
+#
+# Warning and Critical expressed in seconds.  3600sec = 1h, 7200sec = 2h
+define icinga::plugins::checkmediasalsabackup (
+  $pkgname                = $::operatingsystem ? {
+    'centos' => 'nagios-plugins-checkmediasalsabackup',
+    'debian' => 'nagios-plugin-checkmediasalsabackup',
+  },
+  $notification_period    = $::icinga::notification_period,
+  $notifications_enabled  = $::icinga::notifications_enabled,
+  $host_name              = $::fqdn,
+  $warning                = '3600',
+  $critical               = '86400',
+) {
+
+  require icinga
+
+  if $icinga::client {
+
+    if ! defined(Package[$pkgname]) {
+      package{$pkgname:
+        ensure => 'installed',
+      }
+    }
+
+    file{"${::icinga::includedir_client}/check_mediasalsa_backup_${title}.cfg":
+      ensure  => 'file',
+      mode    => '0644',
+      owner   => $::icinga::client_user,
+      group   => $::icinga::client_group,
+      content => "command[check_mediasalsa_backup_${title}]=sudo ${::icinga::plugindir}/check_mediasalsa_backup -w ${warning} -c ${critical}\n",
+      notify  => Service[$::icinga::service_client],
+    }
+
+    @@nagios_service{"check_mediasalsa_backup_${host_name}_${title}":
+      check_command         => "check_nrpe_command!check_mediasalsa_backup_${title}",
+      service_description   => "Check Mediasalsa Backups ${host_name}",
+      host_name             => $host_name,
+      use                   => 'generic-service',
+      notification_period   => $notification_period,
+      notifications_enabled => $notifications_enabled,
+      target                => "${::icinga::targetdir}/services/${host_name}.cfg",
+    }
+  }
+}
