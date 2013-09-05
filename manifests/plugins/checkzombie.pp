bf346500136ef5203bedfc1aba79b07716f093f5
Laurent Bigonville <bigon@bigon.be>
2013-09-05 17:03:47 +0200

    Add host_name parameter

--- manifests/plugins/checkzombie.pp
+++ manifests/plugins/checkzombie.pp
@@ -13,6 +13,7 @@ class icinga::plugins::checkzombie (
     @@nagios_service { "check_zombie_procs_${::fqdn}":
       check_command         => 'check_nrpe_command!check_zombie_procs',
       service_description   => 'Zombie processes',
+      host_name             => $::fqdn,
       notification_period   => $notification_period,
       notifications_enabled => $notifications_enabled,
       target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
