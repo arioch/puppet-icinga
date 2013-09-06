c2fe850c7e3c4d4b029201fe90c5ba0fad2d7806
Laurent Bigonville <bigon@bigon.be>
2013-09-06 16:11:37 +0200

    Drop populating the database from the module
    This should be removed from the icinga modules as other types of
    database (!mysql) could be user.

--- manifests/plugins/icingaweb/config.pp
+++ manifests/plugins/icingaweb/config.pp
@@ -19,18 +19,8 @@ class icinga::plugins::icingaweb::config {
     $icinga::params::icingaweb_logdir:
       ensure => directory;
 
-    "${icinga::params::icingaweb_confdir}/etc/schema/mysql.sql":
-      ensure   => present,
-      notify   => Exec['icinga_web-db-initialize'];
-
     '/etc/icinga-web/conf.d/databases.xml':
       ensure   => present,
       content  => template('icinga/plugins/icingaweb/databases.xml');
-  }
-
-  exec {
-    'icinga_web-db-initialize':
-      unless  => "mysqlshow ${icinga::params::icingaweb_dbname} | grep cronk",
-      command => "mysql -u root icinga_web < ${icinga::params::icingaweb_confdir}/etc/schema/mysql.sql";
   }
 }
