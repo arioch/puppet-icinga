e660303d13f04f366307dbb9f038ac0431f189b0
Daniel Paulus <d.paulus@gmail.com>
2014-05-20 17:52:48 +0200

    Fix class parameters without a default value and add a fail statement to catch using the UNSET value

--- manifests/plugins/notifyxmpp.pp
+++ manifests/plugins/notifyxmpp.pp
@@ -3,14 +3,30 @@
 # This class provides an xmpp notification plugin.
 #
 class icinga::plugins::notifyxmpp (
-  $xmpp_client,
-  $xmpp_jid,
-  $xmpp_server,
-  $xmpp_auth,
-  $xmpp_port = '5222'
+  $xmpp_client = 'UNSET',
+  $xmpp_jid    = 'UNSET',
+  $xmpp_server = 'UNSET',
+  $xmpp_auth   = 'UNSET',
+  $xmpp_port   = '5222'
 ) {
 
   require ::icinga
+
+  if $xmpp_client == 'UNSET' {
+    fail('You should provide an xmpp_client but did not set the var')
+  }
+
+  if $xmpp_jid == 'UNSET' {
+    fail('You should provide an xmpp_jid but did not set the var')
+  }
+
+  if $xmpp_server == 'UNSET' {
+    fail('You should provide an xmpp_server but did not set the var')
+  }
+
+  if $xmpp_auth == 'UNSET' {
+    fail('You should provide an xmpp_auth but did not set the var')
+  }
 
   if $icinga::server {
     file { "${::icinga::plugindir}/notify_via_jabber":
