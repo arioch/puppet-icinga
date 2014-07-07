a100d2f42097cadc37931194b681e67baeeb17bb
Patrick Van Brussel <patrick@inuits.eu>
2014-07-07 14:40:14 +0200

    Fixed typo
    Signed-off-by: Patrick Van Brussel <patrick@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -34,7 +34,7 @@ class icinga::params {
   $export_resources              = true
   $logdir_client                 = '/var/log/nrpe'
   $logdir_server                 = '/var/log/icinga'
-  $set_expire_ack_by_default     = '0',
+  $set_expire_ack_by_default     = '0'
 
   case $::operatingsystem {
     'Debian', 'Ubuntu': {
