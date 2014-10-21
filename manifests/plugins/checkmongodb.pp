4b70b4277206c5d7a70126764f844dbfcb32f998
kayn <pulecp@gmail.com>
2014-10-21 09:21:46 +0200

    condition if graphite host exists

--- manifests/plugins/checkmongodb.pp
+++ manifests/plugins/checkmongodb.pp
@@ -12,6 +12,7 @@ class icinga::plugins::checkmongodb (
   $mongod_bind_ip               = '127.0.0.1',
   $mongod_graphite_io_read_url  = 'http://graphite/render?target=mongo_host.processes.mongod.ps_disk_octets.read&from=-5minutes&rawData=true',
   $mongod_graphite_io_write_url = 'http://graphite/render?target=mongo_host.processes.mongod.ps_disk_octets.write&from=-5minutes&rawData=true',
+  $graphite_host                = undef,
 ) inherits icinga {
 
   if $icinga::client {
@@ -114,26 +115,28 @@ class icinga::plugins::checkmongodb (
       target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
     }
 
-    @@nagios_service{"check_mongod_io_read_operations${::fqdn}":
-      check_command         => "check_graphite!${mongod_graphite_io_read_url}!10000000!50000000",
-      service_description   => 'MongoDB IO Read Operations',
-      host_name             => $::fqdn,
-      use                   => 'generic-service',
-      contact_groups        => $contact_groups,
-      notification_period   => $notification_period,
-      notifications_enabled => $notifications_enabled,
-      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
-    }
+    if $graphite_host != undef {
+      @@nagios_service{"check_mongod_io_read_operations${::fqdn}":
+        check_command         => "check_graphite!${mongod_graphite_io_read_url}!10000000!50000000",
+        service_description   => 'MongoDB IO Read Operations',
+        host_name             => $::fqdn,
+        use                   => 'generic-service',
+        contact_groups        => $contact_groups,
+        notification_period   => $notification_period,
+        notifications_enabled => $notifications_enabled,
+        target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+      }
 
-    @@nagios_service{"check_mongod_io_write_operations${::fqdn}":
-      check_command         => "check_graphite!${mongod_graphite_io_write_url}!10000000!50000000",
-      service_description   => 'MongoDB IO Write Operations',
-      host_name             => $::fqdn,
-      use                   => 'generic-service',
-      contact_groups        => $contact_groups,
-      notification_period   => $notification_period,
-      notifications_enabled => $notifications_enabled,
-      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+      @@nagios_service{"check_mongod_io_write_operations${::fqdn}":
+        check_command         => "check_graphite!${mongod_graphite_io_write_url}!10000000!50000000",
+        service_description   => 'MongoDB IO Write Operations',
+        host_name             => $::fqdn,
+        use                   => 'generic-service',
+        contact_groups        => $contact_groups,
+        notification_period   => $notification_period,
+        notifications_enabled => $notifications_enabled,
+        target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+      }
     }
   }
 }
