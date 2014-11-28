4ca31742d276bed6f48722155f4ad435bedf436e
Pavel Pulec <kayn@inuits.eu>
2014-11-28 16:29:56 +0100

    adopted elasticsearch to NRPE
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- manifests/plugins/checkelasticsearch.pp
+++ manifests/plugins/checkelasticsearch.pp
@@ -4,18 +4,16 @@
 #
 class icinga::plugins::checkelasticsearch (
   $pkgname               = 'nagios-plugin-elasticsearch',
-) {
+) inherits icinga {
 
-  require icinga
-
-  if !defined(Package['python-pip']){
-    package{'python-pip':
-      ensure => present,
+  if $icinga::client {
+    if !defined(Package['python-pip']){
+      package{'python-pip':
+        ensure => present,
+      }
     }
-  }
 
-  if $icinga::server {
-    if ! defined(Package[$pkgname]) {
+    if !defined(Package[$pkgname]) {
       package{$pkgname:
         ensure   => present,
         provider => 'pip',
@@ -23,10 +21,24 @@ class icinga::plugins::checkelasticsearch (
       }
     }
 
-    nagios_command{'check_elasticsearch':
-      ensure       => present,
-      command_line => '$USER1$/check_elasticsearch -H $ARG1$ -p $ARG2$',
-      target       => "${::icinga::targetdir}/commands/check_elasticsearch.cfg",
+    file { "${::icinga::includedir_client}/elasticsearch.cfg":
+      ensure  => 'file',
+      mode    => '0644',
+      owner   => $::icinga::client_user,
+      group   => $::icinga::client_group,
+      content => 'command[check_elasticsearch]=/usr/bin/check_elasticsearch',
+      notify  => Service[$::icinga::service_client],
+    }
+
+    @@nagios_service{"check_elasticsearch_status_${::fqdn}":
+      check_command         => "check_nrpe!check_elasticsearch",
+      service_description   => "Elasticsearch status",
+      host_name             => $::fqdn,
+      contact_groups        => $::environment,
+      use                   => 'generic-service',
+      notification_period   => $::icinga::notification_period,
+      notifications_enabled => $::icinga::notifications_enabled,
+      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
     }
   }
 }
