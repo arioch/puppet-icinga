ca8c41cc8d7dc35702563ee610fe5e87b653ee2d
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2014-12-15 17:56:19 +0100

    checkjstat: nagios user, not group
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/plugins/checkjstat.pp
+++ manifests/plugins/checkjstat.pp
@@ -32,7 +32,7 @@ define icinga::plugins::checkjstat (
     include ::sudo
     if !defined(Sudo::Conf['nrpe-jstat']) {
       sudo::conf{'nrpe-jstat':
-        content => '%nagios ALL=(root) NOPASSWD:/usr/bin/jstat'
+        content => 'nagios ALL=(root) NOPASSWD:/usr/bin/jstat'
       }
     }
 
