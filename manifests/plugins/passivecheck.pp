0a5e4d335737372fe937033900789e64be688d63
Pavel Pulec <kayn@inuits.eu>
2014-12-18 10:54:03 +0100

    passivecheck changed from class to define
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- manifests/plugins/passivecheck.pp
+++ manifests/plugins/passivecheck.pp
@@ -1,24 +1,29 @@
-class icinga::plugins::passivecheck(
-  $services = undef,
-  $freshness_threshold = '3600',
+# == Define: icinga::plugins::passivecheck
+#
+# This define provides a passivecheck plugin.
+#
+# Usage:
+#   $services = [ 'collectd', 'crond' ]
+#   icinga::plugins::passivecheck { $services:}
+
+define icinga::plugins::passivecheck(
+  $service_description = $title,
+  $unique_id           = "${title}-${::fqdn}",
+  $freshness_threshold = 3600,
 ){
 
-  define passive_checks_from_array() {
-    @@nagios_service{ "${title}-${::fqdn}":
-      active_checks_enabled  => 0,
-      check_freshness        => 1,
-      freshness_threshold    => $freshness_threshold,
-      notifications_enabled  => $::icinga::notifications_enabled,
-      passive_checks_enabled => 1,
-      service_description    => $title,
-      host_name              => $::fqdn,
-      use                    => 'generic-service',
-      target                 => "${::icinga::targetdir}/services/${::fqdn}.cfg",
-      check_command          => "check_dummy!0 \"check was refreshed after\
+  @@nagios_service{ $unique_id:
+    active_checks_enabled  => 0,
+    check_freshness        => 1,
+    freshness_threshold    => $freshness_threshold,
+    notifications_enabled  => $::icinga::notifications_enabled,
+    passive_checks_enabled => 1,
+    service_description    => $service_description,
+    host_name              => $::fqdn,
+    use                    => 'generic-service',
+    target                 => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+    check_command          => "check_dummy!0 \"check was refreshed after \
 ${freshness_threshold} seconds\"",
-    }
   }
-
-  passive_checks_from_array { $services: }
 
 }
