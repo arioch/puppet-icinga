3fd17bf3c9a5257f9a32fcecd951b013df09d80d
Jan Tlusty <honza@inuits.eu>
2015-01-16 10:20:04 +0000

    deleted old haproxy template
    Signed-off-by: Jan Tlusty <honza@inuits.eu>

--- templates/plugins/haproxy.cfg.rb
+++
@@ -1,6 +0,0 @@
-### File managed with puppet ###
-### Served by:        '<%= scope.lookupvar('::servername') %>'
-### Module:           '<%= scope.to_hash['module_name'] %>'
-### Template source:  '<%= template_source %>'
-
-command[check_haproxy]=<%= @plugindir %>/check_haproxy.rb -u localhost -U <%= @username %> -P <%= @password %>
