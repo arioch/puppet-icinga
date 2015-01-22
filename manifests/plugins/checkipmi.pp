1ba6382b3c62295b46407e3d404c436792d65f3b
honza <honza@inuits.eu>
2015-01-22 12:24:15 +0000

    added hiera lookup for ignored sensors for ipmi icinga check
    Signed-off-by: honza <honza@inuits.eu>

--- manifests/plugins/checkipmi.pp
+++ manifests/plugins/checkipmi.pp
@@ -8,7 +8,7 @@ class icinga::plugins::checkipmi (
   $max_check_attempts           = $::icinga::max_check_attempts,
   $notification_period          = $::icinga::notification_period,
   $notifications_enabled        = $::icinga::notifications_enabled,
-
+  $ignored_sensors              = hiera('ignored_sensors'),
 ) inherits icinga {
 
    package { 'perl-IPC-Run.noarch':
