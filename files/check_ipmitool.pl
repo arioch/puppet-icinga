faa5e3a99ba399aab92843524221e3f4847f8f51
honza <honza@inuits.eu>
2015-01-30 10:56:53 +0000

    added icinga ipmitool chassis  check refs #2937 @10.55
    Signed-off-by: honza <honza@inuits.eu>

---
+++ files/check_ipmitool.pl
@@ -0,0 +1,112 @@
+#!/usr/bin/perl -w
+#
+
+use strict;
+use Getopt::Long;
+use Nagios::Plugin;
+
+my ( $ip, $user, $pwd );
+my $np = Nagios::Plugin->new;
+
+GetOptions(
+		'ip=s' => \$ip,
+		'user=s' => \$user,
+		'pwd=s' => \$pwd,
+		);
+
+#my $ipmiopts="";
+#if ( !$ip || !$user || !$pwd ) {
+#	print "Usage:\n".
+#		"$0 --ip=<ip> --user=<user> --pwd=<pwd>\n";
+#	exit 3;
+#} else {
+#	$ipmiopts = "-I lan -H $ip -U $user -P $pwd";
+#}
+
+
+#
+# Main
+#
+
+my $powerfail=0;
+my %result;
+
+my @ipmicmd=`ipmitool power status 2>&1`;
+if ( grep /Error: Unable to establish LAN session/, @ipmicmd) {
+  print "Unable to connect\n";
+  exit 3;
+}
+if ( grep /Could not open device/, @ipmicmd) {
+  print "Running as root?\n";
+  exit 3;
+}
+
+if ( $? != 0 ) {
+  print "Command not found ...\n";
+  exit 3;
+}
+
+# Checks chassis status
+check_chassis();
+
+
+# Exit
+my  ($code, $message) = $np->check_messages();
+$np->nagios_exit( $code, $message );
+
+
+
+
+#
+# SUBS
+#
+
+#
+# Check chassis
+#
+
+
+#System Power         : on
+#Power Overload       : false
+#Power Interlock      : inactive
+#Main Power Fault     : false
+#Power Control Fault  : false
+#Power Restore Policy : always-off
+#Last Power Event     : 
+#Chassis Intrusion    : inactive
+#Front-Panel Lockout  : inactive
+#Drive Fault          : false
+#Cooling/Fan Fault    : false
+
+sub check_chassis {
+
+	my @ipmicmd=`ipmitool chassis status 2>&1`;
+
+	my $drive="";
+	my $main="";
+	my $fan="";
+	my $outputchassis="";
+	my $rvchassis=0;
+
+	foreach (@ipmicmd) { 
+		chomp $_;
+		if ( $_ =~ /Invalid command/ ) {
+			# foefel voor 2650
+			return (0,"");
+		}
+		if ( $_ =~ /Drive Fault\s+:\s+(\w+)/ ) { $drive=$1; }
+		if ( $_ =~ /Main Power Fault\s+:\s+(\w+)/ ) { $main=$1; }
+		if ( $_ =~ /Cooling\/Fan Fault\s+:\s+(\w+)/ ) { $fan=$1; }
+	}
+
+	if ( $drive ne "false" ) {
+    $np->add_message( CRITICAL, "Drive fault" );
+	}
+	if ( $main ne "false" ) {
+    $np->add_message( CRITICAL, "Main power fault" );
+	}
+	if ( $fan ne "false" ) {
+    $np->add_message( CRITICAL, "Fan fault" );
+	}
+
+}
---
+++ manifests/plugins/checkipmichassis.pp
@@ -0,0 +1,51 @@
+# == Class: icinga::plugins::checkipmichassis
+#
+# This class provides a checkhipmichassis plugin.
+#
+class icinga::plugins::checkipmichassis (
+  $ensure                       = present,
+  $contact_groups               = $::environment,
+  $max_check_attempts           = $::icinga::max_check_attempts,
+  $notification_period          = $::icinga::notification_period,
+  $notifications_enabled        = $::icinga::notifications_enabled,
+) inherits icinga {
+
+    
+   file { "${::icinga::plugindir}/check_ipmitool.pl":
+      ensure  => present,
+      mode    => '0755',
+      owner   => 'root',
+      group   => 'root',
+      source => 'puppet:///modules/icinga/check_ipmitool.pl',
+      notify  => Service[$icinga::service_client],
+      require => Class['icinga::config'];
+    }
+    file { "${::icinga::includedir_client}/ipmi_chassis.cfg":
+      ensure  => 'file',
+      mode    => '0644',
+      owner   => $::icinga::client_user,
+      group   => $::icinga::client_group,
+      content => template('icinga/plugins/ipmi_chassis.cfg.erb'),
+      notify  => Service[$::icinga::service_client],
+    }
+
+
+
+    @@nagios_service { "ipmi_chassis_status${::fqdn}":
+      check_command         => 'check_nrpe_command!check_ipmi_chassis',
+      service_description   => 'IPMI chassis status',
+      host_name             => $::fqdn,
+      contact_groups        => $contact_groups,
+      notification_period   => $notification_period,
+      notifications_enabled => $notifications_enabled,
+      max_check_attempts    => $max_check_attempts,
+      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+    }
+
+    sudo::conf{'ipmi_chassis_conf':
+    content => "Defaults:nagios !requiretty
+    nagios ALL=(ALL) NOPASSWD:/usr/bin/ipmitool\n",
+    }
+
+  }
+
