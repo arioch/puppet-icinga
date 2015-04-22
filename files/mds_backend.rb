1df55cf18c78fc03c472639104ae452c3f0f81c6
honza <honza@inuits.eu>
2015-04-22 14:12:24 +0000

    the mds_backend.rb script changed so it works with the ruby version installed on the boxes
    Signed-off-by: honza <honza@inuits.eu>

--- files/mds_backend.rb
+++ files/mds_backend.rb
@@ -3,9 +3,18 @@
 require 'open-uri'
 require 'openssl'
 
+#on several salsa instances it wouldnt run as it fails to verify the certificate,
+#therfore i need to disable checking it, and cant think of a better way of doing it with the ancient ruby version currently used(1.8.7).  
+
+original_stderr = $stderr.clone
+$stderr.reopen(File.new('/dev/null', 'w'))
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+$stderr.reopen(original_stderr)
+
+
 lastLine=''
 fails=[]
-open(ARGV[0], :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).each do |line|
+open(ARGV[0]).each do |line|
   if line.match(/^\s+<failure>TRUE<\/failure>\s+$/) and !lastLine.match(/\//)
     fails.push(lastLine.strip[1..-2])
   end
