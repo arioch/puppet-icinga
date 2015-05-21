29bc3b01f15af7348f812694c4d4db9d7d6c7edc
Pavel Pulec <kayn@inuits.eu>
2015-05-21 17:10:29 +0200

    checkntpdhealth - fix ignoring of parameters
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- manifests/plugins/checkntpdhealth.pp
+++ manifests/plugins/checkntpdhealth.pp
@@ -31,9 +31,9 @@ class icinga::plugins::checkntpdhealth (
     mode    => '0644',
     owner   => $::icinga::client_user,
     group   => $::icinga::client_group,
-    content => "command[check_ntpd_health]=${script_path} --warning \
-${warn_value} --critical ${crit_value} --peer_critical ${peer_critical} \
---peer_warning ${peer_warning}\n",
+    content => "command[check_ntpd_health]=${script_path} --warning=\
+${warn_value} --critical=${crit_value} --peer_critical=${peer_critical} \
+--peer_warning=${peer_warning}\n",
     notify  => Service[$::icinga::service_client],
     require => File[$script_path],
   }
