b5fa35e2cb6d6712ed7229325c806b0988d761a4
Patrick Van Brussel <patrick@inuits.eu>
2015-08-06 11:53:39 +0200

    Make sslscan a defined type, so we can include it multiple times on the same host
    Signed-off-by: Patrick Van Brussel <patrick@inuits.eu>

--- manifests/plugins/checksslscan.pp
+++ manifests/plugins/checksslscan.pp
@@ -1,8 +1,8 @@
 # == Class: icinga::plugins::checksslscan
 #
-# This class provides a checksslscan plugin.
+# This defined type provides a checksslscan plugin.
 #
-class icinga::plugins::checksslscan (
+define icinga::plugins::checksslscan (
   $host_url              = undef,
   $host_ip               = undef,
   $warning_grade         = 'B',
@@ -48,14 +48,18 @@ class icinga::plugins::checksslscan (
   }
 
   if $icinga::client {
-    file { "${::icinga::plugindir}/check_sslscan.pl":
-      ensure  => present,
-      mode    => '0755',
-      owner   => 'root',
-      group   => 'root',
-      source  => 'puppet:///modules/icinga/check_sslscan.pl',
-      notify  => Service[$icinga::service_client],
-      require => Class['icinga::config'];
+
+    # Only include this file once
+    if (!defined(File["${::icinga::plugindir}/check_sslscan.pl]")) {
+      file { "${::icinga::plugindir}/check_sslscan.pl":
+        ensure  => present,
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/icinga/check_sslscan.pl',
+        notify  => Service[$icinga::service_client],
+        require => Class['icinga::config'];
+      }
     }
 
     file{"${::icinga::includedir_client}/check_sslscan.cfg":
@@ -63,13 +67,13 @@ class icinga::plugins::checksslscan (
       mode    => '0644',
       owner   => $::icinga::client_user,
       group   => $::icinga::client_group,
-      content => "command[check_sslscan]=${::icinga::plugindir}/check_sslscan.pl -H ${host_name} -w ${warning_grade} -c ${critical_grade} ${_publish_results}${_accept_cached_results}${_max_cache_age}${_ip_address}${_debug_mode}\n",
+      content => "command[check_sslscan_${host_url}]=${::icinga::plugindir}/check_sslscan.pl -H ${host_url} -w ${warning_grade} -c ${critical_grade} ${_publish_results}${_accept_cached_results}${_max_cache_age}${_ip_address}${_debug_mode}\n",
       notify  => Service[$::icinga::service_client],
     }
 
-    @@nagios_service { "check_all_disks_${::fqdn}_${host_name}":
-      check_command         => 'check_nrpe_command!check_all_disks',
-      service_description   => 'Disks',
+    @@nagios_service { "check_sslscan_${::fqdn}_${host_url}":
+      check_command         => "check_nrpe_command!check_sslscan_${host_url}",
+      service_description   => 'SSL Quality',
       host_name             => $::fqdn,
       contact_groups        => $contact_groups,
       max_check_attempts    => $max_check_attempts,
