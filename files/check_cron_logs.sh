67fcf273379512311cf602db83fe196b4519f693
honza <honza@inuits.eu>
2015-08-13 09:56:15 +0200

    add the check_cron_logs class refs #15477
    Signed-off-by: honza <honza@inuits.eu>

---
+++ files/check_cron_logs.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+REGEX="("$(echo "$@" | sed 's/ /)|(/g')")"
+#echo $REGEX
+LOG_FILE='/var/log/messages'
+DATE=$(date --date="24 hours ago" '+%b %-d %H')
+NAME=$(hostname --short)
+CRONS_FAILING=''
+
+##By defualt, only logs newer than 24h are checked, let's check if there even are that old logs
+
+if grep -q "$DATE" /var/log/messages; then
+  if [ $REGEX != '()' ]; then
+    CRONS_FAILING=$(cat $LOG_FILE | sed "1,/$DATE/d" |egrep "cron .*\[[0-9]+\].*\[error\]" |
+    sed -r "s/^.*$NAME [^ ]+ cron ([^\[]+).*/\1/g" | sort | uniq | egrep -v $REGEX)
+  else
+    CRONS_FAILING=$(cat $LOG_FILE | sed "1,/$DATE/d" |egrep "cron .*\[[0-9]+\].*\[error\]" |
+    sed -r "s/^.*$NAME [^ ]+ cron ([^\[]+).*/\1/g" | sort | uniq)
+  fi
+
+##if not, we're simply reading the whole file
+
+else
+  if [ $REGEX != '()' ]; then
+    CRONS_FAILING=$(cat $LOG_FILE |egrep "cron .*\[[0-9]+\].*\[error\]" |
+    sed -r "s/^.*$NAME [^ ]+ cron ([^\[]+).*/\1/g" | sort | uniq | egrep -v $REGEX)
+  else
+    CRONS_FAILING=$(cat $LOG_FILE |egrep "cron .*\[[0-9]+\].*\[error\]" |
+    sed -r "s/^.*$NAME [^ ]+ cron ([^\[]+).*/\1/g" | sort | uniq)
+  fi
+fi
+
+if [ -z "$CRONS_FAILING" ]; then
+ echo "No cron jobs have failed over the past 24 hours."
+ exit 0
+else
+ echo "Cron jobs that have failed over the past 24 hours: "$CRONS_FAILING
+ exit 1
+fi
+
