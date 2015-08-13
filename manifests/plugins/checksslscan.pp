6e50b2d6063952a03b67c9ab18685bf312d7b0eb
Tom Ritserveldt <muffin@inuits.be>
2015-08-13 11:14:16 +0200

    fixed a copy past error.

--- manifests/plugins/checksslscan.pp
+++ manifests/plugins/checksslscan.pp
@@ -112,7 +112,7 @@ define icinga::plugins::checksslscan (
     }
 
     cron { "check-ssl-${host_url}":
-      command  => "${::icinga::plugindir}/check_sslscan.pl -H ${host_url} -w ${warning_grade} -c ${critical_grade} ${_publish_results}${_accept_cached_results}${_max_cache_a    ge}${_ip_address}${_debug_mode}\n",
+      command  => "${::icinga::plugindir}/check_sslscan.pl -H ${host_url} -w ${warning_grade} -c ${critical_grade} ${_publish_results}${_accept_cached_results}${_max_cache_age}${_ip_address}${_debug_mode}\n",
       user     => $::icinga::client_user,
       month    => '*',
       monthday => '*',
