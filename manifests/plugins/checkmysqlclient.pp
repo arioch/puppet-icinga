b99e3a01d39c70480ef8eda01170405f3e114b1d
Pavel Pulec <kayn@inuits.eu>
2015-09-22 11:19:26 +0200

    change variables for mysqlclient corresponding to percona::rights
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- manifests/plugins/checkmysqlclient.pp
+++ manifests/plugins/checkmysqlclient.pp
@@ -3,29 +3,33 @@
 # This class provides a checkmysqld plugin.
 #
 class icinga::plugins::checkmysqlclient (
-  $db_name,
-  $db_host,
-  $db_user,
-  $db_pass,
-  $ensure                = present,
+  $database,
+  $host,
+  $user,
+  $password,
   $contact_groups        = $::environment,
   $max_check_attempts    = $::icinga::max_check_attempts,
   $notification_period   = $::icinga::notification_period,
   $notifications_enabled = $::icinga::notifications_enabled,
 ) inherits icinga {
 
-  file { "${::icinga::includedir_client}/mysql_client_${db_name}.cfg":
+  file { "${::icinga::includedir_client}/mysql_client_${database}.cfg":
     ensure  => 'file',
     mode    => '0644',
     owner   => $::icinga::client_user,
     group   => $::icinga::client_group,
     notify  => Service[$::icinga::service_client],
-    content => "command[check_mysql_${db_name}]=/usr/lib64/nagios/plugins/check_mysql -H ${db_host} -u ${db_user} -p ${db_pass} -d ${db_name}"
+    content => "command[check_mysql_${database}]=/usr/lib64/nagios/plugins/check_mysql -H ${host} -u ${user} -p ${password} -d ${database}"
   }
 
-  @@nagios_service { "check_mysql_client_${::fqdn}_${db_name}":
-    check_command       => "check_nrpe_command!check_mysql_${db_name}",
-    service_description => "mysql client db: ${db_name}",
+  @@nagios_service { "check_mysql_client_${::fqdn}_${database}":
+    check_command         => "check_nrpe_command!check_mysql_${database}",
+    service_description   => "mysql client db: ${database}",
+    contact_groups        => $contact_groups,
+    host_name             => $::fqdn,
+    max_check_attempts    => $max_check_attempts,
+    notification_period   => $notification_period,
+    notifications_enabled => $notifications_enabled,
   }
 
 }
