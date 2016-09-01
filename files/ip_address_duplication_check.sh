2065e1b8c1ce395f74ac67ad7865d9cc506a7530
Pavel Pulec <kayn@inuits.eu>
2016-09-01 10:30:35 +0200

    fix ip_address_duplication_check.sh for boxes with more ifaces refs #23350
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- files/ip_address_duplication_check.sh
+++ files/ip_address_duplication_check.sh
@@ -4,14 +4,18 @@
 
 # ignore localhost, and addresses with subnet /32 (because of Hetzner failover IP)
 ips=$(ip addr show | grep "inet\b" |  awk '{print $2}' | grep -E -v '127\.0\.0\.1|\/32' | cut -d/ -f1)
+interfaces=$(ls /sys/class/net/)
 duplications=''
 
 arping=$(which arping) || { echo 'UNKNOWN - arping command not found'; exit 3; }
 
 for ip in $ips
 do
-  $arping -q -D -c 1 "$ip" &>/dev/null
-  [[ $? -ne 0 ]] && duplications="${duplications}${ip} "
+  for iface in $interfaces
+  do
+    $arping -q -D -c 1 -I "$iface" "$ip" &>/dev/null
+    [[ $? -ne 0 ]] && duplications="${duplications}${ip} "
+  done
 done
 
 if [[ -z "$duplications" ]]; then
