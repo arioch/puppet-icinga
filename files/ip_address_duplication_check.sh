de3029871ea844f0acb1b97bc899747fb473bdfd
Pavel Pulec <kayn@inuits.eu>
2016-09-01 11:09:50 +0200

    ip_address_duplication_check.sh sent output to icinga, ignore localhost
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- files/ip_address_duplication_check.sh
+++ files/ip_address_duplication_check.sh
@@ -4,8 +4,9 @@
 
 # ignore localhost, and addresses with subnet /32 (because of Hetzner failover IP)
 ips=$(ip addr show | grep "inet\b" |  awk '{print $2}' | grep -E -v '127\.0\.0\.1|\/32' | cut -d/ -f1)
-interfaces=$(ls /sys/class/net/)
+interfaces=$(ls /sys/class/net/ | grep -v '^lo$')
 duplications=''
+arping_output=''
 
 arping=$(which arping) || { echo 'UNKNOWN - arping command not found'; exit 3; }
 
@@ -13,15 +14,15 @@ for ip in $ips
 do
   for iface in $interfaces
   do
-    $arping -q -D -c 1 -I "$iface" "$ip" &>/dev/null
+    arping_output="${arping_output}\n\n$($arping -D -c 1 -I "$iface" "$ip")"
     [[ $? -ne 0 ]] && duplications="${duplications}${ip} "
   done
 done
 
 if [[ -z "$duplications" ]]; then
-  echo "OK - No duplicate address found."
+  echo -e "OK - No duplicate address found.${arping_output}"
   exit 0
 else
-  echo "CRITICAL - Found duplicate addresses: ${duplications}!!!"
+  echo -e "CRITICAL - Found duplicate addresses: ${duplications}!!!${arping_output}"
   exit 2
 fi
