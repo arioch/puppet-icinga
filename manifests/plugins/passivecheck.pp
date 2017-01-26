fc19a74eed17ba78d293c0945c455712b6da8c06
Tom Van Berlo <tomvanberlo@inuits.eu>
2017-01-26 16:57:53 +0100

    enable notifiction period in passivecheck refs #26372

--- manifests/plugins/passivecheck.pp
+++ manifests/plugins/passivecheck.pp
@@ -10,13 +10,18 @@ define icinga::plugins::passivecheck(
   $service_description = $title,
   $unique_id           = "${title}-${::fqdn}",
   $freshness_threshold = 3600,
+  $contact_groups        = $::environment,
+  $notification_period   = $::icinga::notification_period,
+  $notifications_enabled = $::icinga::notifications_enabled,
 ){
 
   @@nagios_service{ $unique_id:
     active_checks_enabled  => 0,
     check_freshness        => 1,
     freshness_threshold    => $freshness_threshold,
-    notifications_enabled  => $::icinga::notifications_enabled,
+    notifications_enabled  => $notifications_enabled,
+    notification_period    => $notification_period,
+    contact_groups         => $contact_groups,
     passive_checks_enabled => 1,
     service_description    => $service_description,
     host_name              => $::fqdn,
