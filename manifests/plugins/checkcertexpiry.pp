bd30cfa85c0121127663c20c6788464779aea807
honza <honza@inuits.eu>
2017-01-31 13:40:21 +0100

    checkcertexpiry.pp execute the check with sudo refs #26044
    Signed-off-by: honza <honza@inuits.eu>

--- manifests/plugins/checkcertexpiry.pp
+++ manifests/plugins/checkcertexpiry.pp
@@ -36,3 +36,9 @@ define icinga::plugins::checkcertexpiry (
 
   }
 
+  sudo::conf{'ssl_cert_expity':
+    content => "Defaults:nagios !requiretty
+    nagios ALL=(ALL) NOPASSWD:/usr/lib64/nagios/plugins/check_ssl-cert\n",
+  }
+
+
